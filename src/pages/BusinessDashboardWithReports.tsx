eaa81f1a68bf826c3438961c51b190efa48b6b64
jackphi2023 <jackphi2023@gmail.com>
2026-07-21 21:46:31 +0700

    feat(report): attach lazy inline viewer to Business dashboard

--- src/pages/BusinessDashboardWithReports.tsx
+++ src/pages/BusinessDashboardWithReports.tsx
@@ -1,11 +1,13 @@
 import BusinessDashboard from './BusinessDashboard';
 import BusinessReportPanelPortal from '../features/businessReports/BusinessReportPanelPortal';
+import BusinessReportViewerPortal from '../features/businessReports/BusinessReportViewerPortal';
 
 export default function BusinessDashboardWithReports() {
   return (
     <>
       <BusinessDashboard />
       <BusinessReportPanelPortal />
+      <BusinessReportViewerPortal />
     </>
   );
 }
