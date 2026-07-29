f9399fb6d0c3462c09276344760cf3a1595309eb
jackphi2023 <jackphi2023@gmail.com>
2026-07-29 12:20:45 +0700

    Gate Investor routes for guests and unsupported roles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
+import InvestorAccessBoundary from './components/InvestorAccessBoundary';
 import AdminReportsPortal from './features/adminReports/AdminReportsPortal';
 import DashboardProfileReviewNoticePortal from './features/dashboardReview/DashboardProfileReviewNoticePortal';
 import './styles/index.css';
@@ -11,10 +12,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
-        <AdminReportsPortal />
-        <DashboardProfileReviewNoticePortal />
+        <InvestorAccessBoundary>
+          <App />
+          <AdminReportsPortal />
+          <DashboardProfileReviewNoticePortal />
+        </InvestorAccessBoundary>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
