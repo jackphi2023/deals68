98abfb60887c59c6ec314988048cb266bccc9cc3
jackphi2023 <jackphi2023@gmail.com>
2026-08-06 20:52:09 +0700

    Route Advisor Session 5 admin review

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import InvestorAccessBoundary from './components/InvestorAccessBoundary';
@@ -8,14 +8,23 @@ import AdminReportsPortal from './features/adminReports/AdminReportsPortal';
 import BusinessPublicRevenueControls from './features/businessRevenueVisibility/BusinessPublicRevenueControls';
 import PublicBusinessRevenuePresentation from './features/businessRevenueVisibility/PublicBusinessRevenuePresentation';
 import DashboardProfileReviewNoticePortal from './features/dashboardReview/DashboardProfileReviewNoticePortal';
+import AdminAdvisorIntakes from './pages/AdminAdvisorIntakes';
 import './styles/index.css';
+
+function Deals68RouteRoot() {
+  const location = useLocation();
+  if (location.pathname === '/admin/advisor-intakes') {
+    return <AdminAdvisorIntakes />;
+  }
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <InvestorAccessBoundary>
-          <App />
+          <Deals68RouteRoot />
           <AdminReportsPortal />
           <BusinessPublicRevenueControls />
           <PublicBusinessRevenuePresentation />
